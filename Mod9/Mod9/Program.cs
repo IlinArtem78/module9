c5d5c986d3fd1bfee0fc53393039a86531b1f522
IlinArtem78 <artem.ilin1994@gmail.com>
2024-07-22 12:03:38 +0300

    9.3.13

--- Mod9/Mod9/Program.cs
+++ Mod9/Mod9/Program.cs
@@ -1,47 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more informatio
 
-
-
-
-
-
 class Program
 {
+    delegate int RandomNumberDelegate();
     static void Main(string[] args)
     {
-        try
-        {
-          //  Console.WriteLine("Выберите способ для вызова делегата 1 или 2"); 
-          //  byte Num = byte.Parse(Console.ReadLine());
-
-            MyDelegate f1 = F2;
-            f1 += F3;
-
-           int p = f1.Invoke(10, 7);
-           
-           Console.ReadKey();
-        }
-
-        catch (Exception ex)
-        {
-
-            Console.WriteLine(ex.GetType());
-        }
+       
     }
 
-    public delegate int MyDelegate(int x, int y);
+    
 
 
-    static int F2(int x, int y)
-    {
-        Console.WriteLine("Разноасть {0}", x-y);
-        return x - y;
-    }
-    static int F3(int x, int y)
-    {
-        Console.WriteLine("Сумма {0}", x + y);
-        return x + y;
-    }
+
+
 
 }
+
+
+
+
 
