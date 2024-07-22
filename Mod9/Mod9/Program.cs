e0224091928e4049eaf910d660483c9579880e7a
IlinArtem78 <artem.ilin1994@gmail.com>
2024-07-22 09:38:03 +0300

    9.1.3 и 9.1.4

--- Mod9/Mod9/Program.cs
+++ Mod9/Mod9/Program.cs
@@ -1,50 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more informatio
 
+class MyClass : Exception { 
 
-
-
-
-
-
-
-
-using System.Security.Principal;
-
-public class MyException : Exception
-{
-    public MyException()
-    { }
-
-    public MyException(string message)
-        : base(message)
-    { }
-}
-
-public class HumanException : ArgumentException
-{
-    public HumanException(string _exceptionMessage) : base(_exceptionMessage) { }
-  //  public NTAccount AcouuntDTO { get; set; }
-
-
-    public AccountDTO Authenticate(string _userName, string _password)
+    public MyClass()
     {
-        UserEntity findUser = base.FindByUsername(_userName);
-        if (findUser is null) throw new HumanException("Пользователь не найден в системе.");
-        if (findUser.password != _password) throw new HumanException("Пароль не корректный.");
-        return new AccountDTO(findUser, _token.Generate(findUser.id), roleRepository.FindByUserId(findUser.id));
-    }
-}
-
-
-
-public class ExceptionHandler : ActionFilterAttribute, IExceptionFilter
-{
-    readonly Error _error = new Error();
-    public void OnException(ExceptionContext context)
-    {
-        _error.Write(context.Exception.ToString());
+        MyClass myClass = new MyClass();
+        myClass.Data.Add("Дата создания исключения",DateTime.Now);
+        //////////////////////////////////////
+        
+        MyClass myClass2 = new MyClass();
         string errorMessage = "Произошла непредвиденная ошибка в приложении. Администрация сайта уже бежит на помощь.";
-        if (context.Exception is HumanException) errorMessage = context.Exception.Message;
-        context.Result = new BadRequestObjectResult(errorMessage);
+        
+        
+        errorMessage =  myClass2.Message;
+        myClass2.HelpLink = "Помощь в пути";
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());   
+        }     
+    
     }
+
+
 }
+
