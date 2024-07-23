2441a288e0468944406700c57369bcbe51eac5dd
IlinArtem78 <artem.ilin1994@gmail.com>
2024-07-23 17:16:01 +0300

    Практикум 9.6.1. Задание 1.

--- Mod9/Mod9/Program.cs
+++ Mod9/Mod9/Program.cs
@@ -1,49 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more informatio
 
+/*
+Задание 1
+1. Создайте свой тип исключения.
+2. Сделайте массив из пяти различных видов исключений, включая собственный тип исключения. 
+Реализуйте конструкцию TryCatchFinally, в которой будет итерация на каждый тип исключения (блок finally по желанию).
+3.В блоке catch выведите в консольном сообщении текст исключения.
+ 
+ 
+Задание 2 
+
+
+
+
+ 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using Mod9;
+
 class Program
 {
-    
     static void Main(string[] args)
+
     {
-        LexusInfo lexus = BuildLexus;
-        Car C = lexus("RX");             //-ковариантность
-        Console.WriteLine(C.Model);
+        Console.WriteLine("Введите выбор программы для запуска 1 или 2"); 
+        byte num = byte.Parse(Console.ReadLine());  
+        switch(num)
+        {
+            case 1:
+                Task1 task1 = new Task1();
 
+                break;
 
+                case 2:
+                break;
+                default:
+                Console.WriteLine("Вы ввели неверное значение"); 
+                break;
+        }
 
-
-        ParentInform parent = ParentInfo; ///-контравариантности
-        parent.Invoke(new Child());
-
-        Console.Read();
 
     }
-
-    public delegate Lexus LexusInfo(string name); //
-
-    public delegate void ParentInform(Child children); ///
-    private static Lexus BuildLexus(string model) //
-    {
-        return new Lexus { Model = model };
-    }
-    ///
-    public static void ParentInfo(Parent parent)
-    {
-        Console.WriteLine(parent);
-    }
-
 }
-class Car
-{
-    public string Model { get; set; }
-}
-class Lexus : Car {
-
-  
-
-}
-class Parent { }
-class Child : Parent { }
-
 
 
---
+++ Mod9/Mod9/Task1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+Задание 1
+1. Создайте свой тип исключения.
+2. Сделайте массив из пяти различных видов исключений, включая собственный тип исключения. 
+Реализуйте конструкцию TryCatchFinally, в которой будет итерация на каждый тип исключения (блок finally по желанию).
+3.В блоке catch выведите в консольном сообщении текст исключения.
+
+ 
+ */
+namespace Mod9
+{
+
+    public class NoN : DivideByZeroException 
+    {
+    }
+    public class ArgOutOfRange : ArgumentOutOfRangeException { }
+    public class NonDirectory : DirectoryNotFoundException { }
+
+    public class NonFile : FileNotFoundException { }
+
+    public class InvdOperaException : InvalidOperationException { } 
+
+
+
+    public class Task1
+    {
+        public Task1()
+        {
+
+            //объявление классов исключений
+            NoN noN = new NoN();
+            ArgOutOfRange argOutOfRange = new ArgOutOfRange();
+            NonDirectory nonDirectory   = new NonDirectory();
+            NonFile nonFile = new NonFile();
+            InvdOperaException invdOperaException = new InvdOperaException();   
+
+            //создание массива исключений
+            ArrayList arrayList = new ArrayList();
+     
+            arrayList.Add(noN); 
+            arrayList.Add(argOutOfRange); 
+            arrayList.Add(nonDirectory); 
+            arrayList.Add(nonFile); 
+            arrayList.Add(invdOperaException);
+            //////  
+            byte n = 0;
+            while (n < arrayList.Count)
+            {
+                ++n;
+                switch (n)
+                {
+                    case 1:
+                        try
+                        {
+                            throw new NoN();
+
+                        }
+                        catch (Exception ex) when (ex is NoN)
+                        {
+                            Console.WriteLine("На ноль делить нельзя!");
+
+                        }
+
+                        break;
+                    case 2:
+                        try
+                        {
+                            throw new ArgOutOfRange();
+                        }
+                        catch (Exception ex) when (ex is ArgOutOfRange)
+                        {
+                            Console.WriteLine($"Аргумент находится за пределами диапазона допустимых значений.");
+                        }
+
+                        break;
+                    case 3:
+                        try
+                        {
+
+                            throw new NonDirectory();
+                        }
+                        catch (Exception ex) when (ex is NonDirectory)
+                        {
+                            Console.WriteLine($"Недопустимая часть пути к каталогу.");
+                        }
+
+                        break;
+                    case 4:
+                        try
+                        {
+                            throw new NonFile();
+
+                        }
+                        catch (Exception ex) when (ex is NonFile)
+                        {
+                            Console.WriteLine($"Файл не существует.");
+                        }
+
+                        break;
+                    case 5:
+                        try
+                        {
+                            throw new InvdOperaException();
+
+                        }
+                        catch (Exception ex) when (ex is InvdOperaException)
+                        {
+                            Console.WriteLine($"Вызов метода недопустим в текущем состоянии объекта.");
+                        }
+
+                        break;
+
+
+
+                        
+                }
+           
+
+
+
+            }
+               
+
+                
+        }
+
+          
+
+
+
+    }
+} 
+
+ 
+
+
+  
+
+
+    
+
