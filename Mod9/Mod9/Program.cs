4caedf2096f0f171f8983d52c3651cc0a40be071
IlinArtem78 <artem.ilin1994@gmail.com>
2024-07-23 14:09:48 +0300

    Задание 9.4.2

--- Mod9/Mod9/Program.cs
+++ Mod9/Mod9/Program.cs
@@ -2,20 +2,39 @@
 
 class Program
 {
-    delegate int RandomNumberDelegate();
+    
     static void Main(string[] args)
     {
-       
+        LexusInfo lexus = BuildLexus;
+        Car C = lexus("RX");             //ковариантность
+        Console.WriteLine(C.Model);
+
+
+        Console.Read();
+
     }
 
-    
+    public delegate Lexus LexusInfo(string name);
 
 
 
 
+
+    private static Lexus BuildLexus(string model)
+    {
+        return new Lexus { Model = model };
+    }
 
 }
+class Car
+{
+    public string Model { get; set; }
+}
+class Lexus : Car {
 
+  
+
+}
 
 
 
